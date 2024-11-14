fbdd8787b35f77bf6b7ec12ac83c6f77994975a7
Ayush-2302 <135198589+Ayush-2302@users.noreply.github.com>
2024-11-14 20:12:40 +0530

    first commit

--- cmfrontend/src/apiService/httpService.js
+++ cmfrontend/src/apiService/httpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 
 export default httpService;
