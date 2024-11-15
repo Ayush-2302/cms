f0ca42da504285987d0afcd34007ffb7dd4bd805
Ayush-2302 <135198589+Ayush-2302@users.noreply.github.com>
2024-11-15 08:32:55 +0530

    first commit

--- cmfrontend/src/pages/LoginForm.jsx
+++ cmfrontend/src/pages/LoginForm.jsx
@@ -21,7 +21,7 @@ const LoginForm = () => {
   const handleSubmit = async (values, { resetForm }) => {
     const response = await loginApi(values);
     if (response.status === 200) {
-      navigate("/cars");
+      navigate("/");
       resetForm();
       window.location.reload();
     }
